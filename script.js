0f578bc6be5c191152d4aef53cb5c14edcb2edbb
FluroAsh <workashleythompson@gmail.com>
2022-04-29 17:20:01 +1000

    adds CSS gloal variables to style sheet & image element now hides when there is no src provided by JS script

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-// DOM Elements for Manipulation
+// Listen for button click to initiate Fetch process
 const btn = document
   .getElementById('get-recipes')
   .addEventListener('click', getRecipeData);
 const recipeImage = document.getElementById('recipe-img');
-const recipeName = document.getElementById('recipe-name');
-// extendsIngredients
-// Base URL for Fetch Request
+// Base URL for Fetch request
 const baseUrl =
   'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?';
 
+if (recipeImage.src === '') {
+  recipeImage.parentElement.style.display = 'none';
+}
+
 // Core functions
 function populateData(data) {
-  const { image, title } = data.recipes[0];
+  // target DOM elements
+  const recipeName = document.getElementById('recipe-name');
+  recipeImage.parentElement.style.display = ''; // remove the style="none" when we pass it in an image
+
+  const { image, title } = data.recipes[0]; // destructure data
   recipeImage.src = image;
-  console.log(title);
   recipeName.innerText = title;
 }
 
@@ -29,7 +34,7 @@ function getRecipeData() {
   fetch(`${baseUrl}number=1&tags=vegetarian%2Cdessert`, options)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.recipes[0]); // TEMPORARY --> show the objects properties
+      console.log(data.recipes[0]); // TEMPORARY --> console.log the object in the data payload
       populateData(data); // Pass the parsed json (data) to populateData function
     })
     .catch((err) => console.error(err));
